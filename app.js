3d3fb620ff6f680e843dc9fbc9d755cd068e6076
Jonathan Rihan da Silva <jrihan@solarbr.com.br>
2020-02-06 13:52:57 -0300

    Atualizacoes

--- app.js
+++ app.js
@@ -5,11 +5,22 @@
 const pop_size = 10
 const parents = 2
 const mutation_probability = 0.5
-
+const ind_size = 3
 
 //Funcao de numeros aleatorios
 function randint(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sample(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
 }
 
 
@@ -45,18 +56,15 @@ function fitness(ind,max){
 }
 
 function selection_and_crossover(population){
-	scored = [] //População com Scored
-	pop_gen = [] //Scored Organizado
-	var selected
+	var scored = [] //População com Scored
+	var pop_gen = [] //Scored Organizado
+	var selected;
 
 	//Medir Fitness
 	for(var i in population){
-		//console.log(fitness(population[i],255))
 		var j = [Number((fitness(population[i],255)).toFixed(2)),population[i]]
 		scored.push(j)
 	}
-
-	console.log(scored)
 
 	//Organizar Maior-Menor
 	for(var i in scored.sort()){
@@ -65,14 +73,27 @@ function selection_and_crossover(population){
 	
 	scored = pop_gen
 	selected = scored.slice(Object.keys(scored).length-parents)
+/*
+	for(var i = 0; i < Object.keys(population).length; i++){
+		point = randint(1, ind_size - 1)
+		parent = sample(selected,2)
 
-	console.log(scored)
-	return selected;
+		for(var j = 0;j < ind_size; j++){
+			if(i >= point){
+				population[i][j] = parent[0][j]
+			}	
+			else{
+				population[i][j] = parent[1][j]
+			}
+		}	
+	}
 
+*/
+	return population;
+	
 }
-
-
 
 //console.log(population(pop_size))
 //console.log(fitness(individual(0,255),255))
+
 console.log(selection_and_crossover(population(5)))
