2fe165fdc70b9c8e388a37d6701eaa854c80124b
RBioZ <jonatha_rihan@hotmail.com>
2020-02-08 15:19:56 -0300

    Final?

--- app.js
+++ app.js
@@ -24,35 +24,19 @@ function sample(arr, size) {
 }
 //-------------------------------------------------------------------------------
 
-<<<<<<< HEAD
 function individual(ind_size,min, max){
 	ind = []
 	for(var i  = 0; i < ind_size; i++){
 		ind[i] = randint(min,max);
-=======
-
-function individual(size,min, max){
-	var ind = [];
-	for(var i = 0; i < size; i++){
-		ind.push(randint(min,max));
->>>>>>> 832d355aa254de9bcecb1dfbacb131c9717da083
 	}
 	return ind;
 }
 
-<<<<<<< HEAD
 function population(size,ind_size,min, max){
 	var pop = [];
 	for(var i = 0; i < size; i++){
 		//console.log(individual(0,255))
 		pop.push(individual(ind_size,min, max));
-=======
-function population(pop,size,min,max){
-	var j = [];
-	for(var i = 0; i < pop; i++){
-		//console.log(individual(0,255))
-		j.push(individual(size,min,max));
->>>>>>> 832d355aa254de9bcecb1dfbacb131c9717da083
 	}
 	return pop;
 }
@@ -60,7 +44,7 @@ function population(pop,size,min,max){
 //Calculo de Fitness
 function fitness(ind,max){
 
-	console.log(ind)
+	//console.log(ind)
 	var fit_points = 100;
 	var size= ind.length; 
 	var fit = 0;
@@ -76,18 +60,18 @@ function selection_and_crossover(population){
 	var pop_gen = [] //Scored Organizado
 	var selected //Individuos Selecionados;
 
-	console.log("1- Population")
-	console.log(population)
+	//console.log("1- Population")
+	//console.log(population)
 
 	//Medir Fitness
 	for(var i in population){
 		var j = [Number((fitness(population[i],255)).toFixed(2)),population[i]]
 		scored.push(j)
 	}
-	console.log('2- Scored')
-	console.log(scored)
+	//console.log('2- Scored')
+	//console.log(scored)
 
-	console.log(scored)
+	//console.log(scored)
 
 	//console.log(scored)
 
@@ -96,71 +80,61 @@ function selection_and_crossover(population){
 		pop_gen.push(scored[i][1])
 	}
 	
-	console.log('3 - Organization')
-	console.log(pop_gen)
+	//console.log('3 - Organization')
+	//console.log(pop_gen)
 
 	scored = pop_gen
 	selected = scored.slice(Object.keys(scored).length-parents)
 
-<<<<<<< HEAD
-	console.log('4 - Selected (Parents)')
+	//console.log('4 - Selected (Parents)')
 	console.log(selected)
 
-=======
->>>>>>> 832d355aa254de9bcecb1dfbacb131c9717da083
-	for(var i = 0; i < Object.keys(population).length; i++){
+	for(var i = 0; i < population.length; i++){
 
 
 		point = randint(1, ind_size - 1)
-		parent = sample(selected,2)
+		parent = Math.floor(Math.random()*10)
 		
-<<<<<<< HEAD
-		console.log('5 - Point and Parents')
-		console.log(point)
 		console.log(parent)
 		
+
+		if(parent <= 5){
+			parent = []
+			parent[0] = selected[0]
+			parent[1] = selected[1]
+		}
+		else{
+			parent = []
+			parent[0] = selected[1]
+			parent[1] = selected[0]
+		}
+		
+		console.log('\nSelected')
+		console.log(selected)
+		console.log('\nParent')
+		console.log(parent)
+
 		for(var j = 0;j < ind_size; j++){
 			if(j >= point){
 				//console.log(population[i][j])
 				//console.log(parent[0][j])
-				console.log('j: '+j+' Point: '+point)
-=======
-		//console.log(population[i])
-//		console.log(point)
-		//console.log(parent)
-		//console.log('------------------------------------------------')
-		
-		for(var j in escopo){
-			if(escopo[j] >= point){
->>>>>>> 832d355aa254de9bcecb1dfbacb131c9717da083
+				//console.log('j: '+j+' Point: '+point)
 				population[i][j] = parent[0][j]
-				console.log(parent[0])
+				//console.log(parent[0])
 			}	 
 			else{
 				//console.log(population[i][j])
 				//console.log(parent[1][j])
-				console.log('j: '+j+' Point: '+point)
+				//console.log('j: '+j+' Point: '+point)
 				population[i][j] = parent[1][j]
-<<<<<<< HEAD
-				console.log(parent[1])
+				//console.log(parent[1])
 			}
 		}	
 	}
 
-=======
-			}		
-		}		
-	}
-
-
->>>>>>> 832d355aa254de9bcecb1dfbacb131c9717da083
 	return population;
 	
 }
 
 
-<<<<<<< HEAD
-console.log(selection_and_crossover(population(5,3,0,255)))
-=======
-console.log(selection_and_crossover(population(5,3,0,255)))
->>>>>>> 832d355aa254de9bcecb1dfbacb131c9717da083
+console.log(selection_and_crossover(population(5,ind_size,0,255)))
