146bfcafe4844b86a47ee28d2f0e1a42ad903584
RBioZ <jonatha_rihan@hotmail.com>
2020-02-10 13:35:55 -0300

    FINAL

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+/*///////////////////////////////////////////////////////////////////////////////
+					.....:::::GENETIC ALGORITHMS:::::.....
 
+#CREATE BY: JONATHAN RYAN DA SILVA (RBIOZ)
+#DATE: 01/02/2020 - 09/02/2020
+
+*///////////////////////////////////////////////////////////////////////////////
 
 // Variaveis
 
-const pop_size = 10
+const population_size = 10
+const individual_size = 3
 const parents = 2
-const mutation_probability = 0.5
-const ind_size = 3
+const mutation_probability = 0.2
+const min = 0;
+const max = 255;
 
-//Funcao de numeros aleatorios
+//------------------------------------------------------------------------------
 function randint(min, max) { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -22,78 +30,119 @@ function sample(arr, size) {
     }
     return shuffled.slice(0, size);
 }
+//-------------------------------------------------------------------------------
 
-
-function individual(min, max){
-	var ind = {'r':0,'g':0,'b':0};
-	for(var i in ind){
+//Individuo
+function individual(){
+	ind = []
+	for(var i  = 0; i < individual_size; i++){
 		ind[i] = randint(min,max);
 	}
 	return ind;
 }
 
-function population(size){
-	var j = {};
-	for(var i = 0; i < size; i++){
+//Populacao
+function create_population(){
+	var pop = [];
+	for(var i = 0; i < population_size; i++){
 		//console.log(individual(0,255))
-		j[i] = individual(0,255);
+		pop.push(individual());
 	}
-	return j;
+	return pop;
 }
 
-function fitness(ind,max){
-
+//Calculo de Fitness
+function fitness(ind){
 	var fit_points = 100;
-	var size= Object.keys(ind).length; 
+	var size= ind.length; 
 	var fit = 0;
 
-	for(var i in ind){
-		//console.log(ind[i]+" : "+(ind[i]*100)/max)
-		//console.log(((ind[i]*1)/max)*(fit_points/size)+"\n")
+	for(var i = 0; i < size; i++){
 		fit += ((ind[i]*1)/max)*(fit_points/size)
 	}
-	return fit_points-fit;
+	return Number((fit_points-fit).toFixed(2));
 }
 
+//Selecao e Crossover
 function selection_and_crossover(population){
 	var scored = [] //População com Scored
-	var pop_gen = [] //Scored Organizado
-	var selected;
+	var pop_organize = [] //Scored Organizado
+	var selected //Individuos Selecionados;
 
-	//Medir Fitness
+	//Ranquear Individuos
 	for(var i in population){
-		var j = [Number((fitness(population[i],255)).toFixed(2)),population[i]]
+		var j = [Number((fitness(population[i])).toFixed(2)),population[i]]
 		scored.push(j)
 	}
 
-	//Organizar Maior-Menor
+	//Organizar conforme Score
 	for(var i in scored.sort()){
-		pop_gen.push(scored[i][1])
+		pop_organize.push(scored[i][1])
 	}
-	
-	scored = pop_gen
-	selected = scored.slice(Object.keys(scored).length-parents)
-/*
-	for(var i = 0; i < Object.keys(population).length; i++){
-		point = randint(1, ind_size - 1)
-		parent = sample(selected,2)
 
-		for(var j = 0;j < ind_size; j++){
-			if(i >= point){
-				population[i][j] = parent[0][j]
-			}	
+	//Selecionar Parents(Pais)
+	scored = pop_organize
+	selected = scored.slice(Object.keys(scored).length-parents)
+	
+	//Variavel de retorno
+	var new_population = []
+
+	//Criar populacao Vazia
+	for(var p in population){
+		new_population.push([0,0,0])
+	}
+
+	//Crossover no material Genetico
+	for(var i = 0; i < population.length; i++){
+
+		var point = randint(1, individual_size - 1)
+		var parent = sample(selected,2)
+
+		for(var j = 0;j < individual_size; j++){
+
+			if(j < point){
+				new_population[i][j] = parent[0][j]
+			}	 
 			else{
-				population[i][j] = parent[1][j]
+				new_population[i][j] = parent[1][j]
 			}
 		}	
 	}
-
-*/
-	return population;
-	
+	return new_population;
 }
 
-//console.log(population(pop_size))
-//console.log(fitness(individual(0,255),255))
+//Funcao de Mutacao 
+function mutation(population){
+	
+	//Populacao Vazia
+	var new_population = [];
 
-console.log(selection_and_crossover(population(5)))
+	for(var p in population){
+		new_population.push([0,0,0])
+	}
+	for(var x in population){
+		for(var y in population[x]){
+			if(Math.random() < mutation_probability){
+				new_population[x][y] = randint(0,255)
+			}
+			else{
+				new_population[x][y] = population[x][y]
+			}
+		}
+	}
+	return new_population
+}
+
+
+
+population = create_population()
+console.log("População Inicial: ")
+console.log(population)
+
+for(var i = 0; i <= 100; i++){
+	population = selection_and_crossover(population)
+	population = mutation(population)
+}
+
+console.log("População Final: ")
+console.log(population)
